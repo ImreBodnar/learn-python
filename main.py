c28636bdd5a29c9ad5807f24aa3687449eaa2110
ibodnar <ibodnar@cleware.hu>
2026-06-08 17:25:25 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# variables
+calc_to_units= 24 * 60 * 60
+unit_name="seconds"
+
+print(f"20 days are {20 * calc_to_units} {unit_name}.")
+print(f"35 days are {35 * calc_to_units} {unit_name}.")
+print(f"55 days are {55 * calc_to_units} {unit_name}.")
+print(f"95 days are {95 * calc_to_units} {unit_name}.")
