e64d85acddd905fb947be7eb1386e28a64db5718
ibodnar <ibodnar@cleware.hu>
2026-06-09 17:09:59 +0200

    Experimenting

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 # variables
-calc_to_units= 24 * 60 * 60
-unit_name="seconds"
+calc_to_units = 24
+unit_name = "hours"
 
-print(f"20 days are {20 * calc_to_units} {unit_name}.")
-print(f"35 days are {35 * calc_to_units} {unit_name}.")
-print(f"55 days are {55 * calc_to_units} {unit_name}.")
-print(f"95 days are {95 * calc_to_units} {unit_name}.")
+
+def days_to_units(num_of_days):
+    return f"{num_of_days} days are {num_of_days * calc_to_units} {unit_name}."
+
+
+def validate_execute():
+    if user_input.isdigit():
+        user_value = int(user_input)
+        if user_value > 0:
+            calculated_value = days_to_units(user_value)
+            print(f"Calculated value: {calculated_value}")
+        elif user_value == 0:
+            print("it's zero")
+    else:
+        print("Input is not digit!")
+
+
+user_input = input("Enter a number of days: ")
+print(f"Given value: {user_input}")
+
+validate_execute()
