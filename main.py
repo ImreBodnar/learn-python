5edd8d3c421d88990931fc89a85dbd0a45ef062f
Imre Bodnár <bodnar.imre.85@gmail.com>
2026-06-13 19:43:45 +0200

    Added dictionary datatype for input

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-# variables
-calc_to_units = 24
-unit_name = "hours"
+def days_to_units(num_of_days, unit):
+    if unit == "hour":
+        return f"{num_of_days} days are {num_of_days * 24} hours."
+    elif unit == "minute":
+        return f"{num_of_days} days are {num_of_days * 24 * 60} minutes."
+    else:
+        return "Unsupported unit!"
 
 
-def days_to_units(num_of_days):
-    return f"{num_of_days} days are {num_of_days * calc_to_units} {unit_name}."
-
-
-def validate_execute():
+def validate_execute(input_dict):
     try:
-        user_value = int(input_value)
+        user_value = int(input_dict["days"])
         if user_value > 0:
-            calculated_value = days_to_units(user_value)
+            calculated_value = days_to_units(user_value, input_dict["unit"])
             print(f"Calculated value: {calculated_value}")
         elif user_value == 0:
             print("It's zero")
@@ -23,8 +23,11 @@ def validate_execute():
 
 user_input = ""
 while user_input != "exit":
-    user_input = input("Enter a number of days: ")
-    print(f"Given value: {user_input.split(',')}")
-    print(f"DataType: {type(user_input.split(','))}")
-    for input_value in set(user_input.split(',')):
-        validate_execute()
+    user_input = input("Enter a number of days and a unit: ")
+    days_and_units = user_input.split(':')
+    print(days_and_units)
+
+    days_and_units_dict = {"days": days_and_units[0], "unit": days_and_units[1]}
+    print(days_and_units_dict)
+
+    validate_execute(days_and_units_dict)
