4418d034c454953bcba7c1d3278b8f590900ab59
ibodnar <ibodnar@cleware.hu>
2026-06-19 18:02:27 +0200

    Made ec2-status-checker to be scheduled

--- EC2-Status-Checker/ec2-status-checker.py
+++ EC2-Status-Checker/ec2-status-checker.py
@@ -1,12 +1,21 @@
 import boto3
+import schedule
 
 ec2_client = boto3.client('ec2')
 ec2_resource = boto3.resource('ec2')
 
-statuses = ec2_client.describe_instance_status()
-for status in statuses['InstanceStatuses']:
-    instance_status = status['InstanceStatus']['Status']
-    system_status = status['SystemStatus']['Status']
-    state = status['InstanceState']['Name']
-    print(
-        f"Instance {status['InstanceId']} is {state} with instance status {instance_status} and system status {system_status}.")
+
+def check_instance_statuses():
+    statuses = ec2_client.describe_instance_status(IncludeAllInstances=True)
+    for status in statuses['InstanceStatuses']:
+        instance_status = status['InstanceStatus']['Status']
+        system_status = status['SystemStatus']['Status']
+        state = status['InstanceState']['Name']
+        print(f"Instance {status['InstanceId']} is {state} with instance status {instance_status} and system status {system_status}.")
+    print("##########################################\n")
+
+
+schedule.every(5).seconds.do(check_instance_statuses)
+
+while True:
+    schedule.run_pending()
