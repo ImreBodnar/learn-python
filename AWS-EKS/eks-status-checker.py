8a2adbaf0a78b1d69fc5a33d0d30ac52a6ea9c78
Imre Bodnár <bodnar.imre.85@gmail.com>
2026-06-20 21:04:35 +0200

    Added eks-status-checker.py

---
+++ AWS-EKS/eks-status-checker.py
@@ -0,0 +1,20 @@
+import boto3
+
+client = boto3.client('eks')
+clusters = client.list_clusters()['clusters']
+
+for cluster in clusters:
+    print(f"ClusterName: {cluster}")
+
+    response = client.describe_cluster(name=cluster)
+
+    cluster_info = response['cluster']
+    cluster_status = cluster_info['status']
+    cluster_endpoint = cluster_info['endpoint']
+    cluster_version = cluster_info['version']
+
+    print(f"ClusterStatus: {cluster_status}")
+    print(f"ClusterEndpoint: {cluster_endpoint}")
+    print(f"ClusterVersion: {cluster_version}")
+
+    print("###################################################")
